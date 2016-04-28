8fccc620953b6813c3b33c3b86da3dac1d6f6dbf
Rohit Sinha <rsinha@berkeley.edu>
2016-04-28 09:17:14 -0700

    adding script for copying boogie refs

---
+++ references/copy_boogie_libs.bat
@@ -0,0 +1,35 @@
+copy %1\BoogieAbsInt.dll .
+copy %1\BoogieAbsInt.pdb .
+copy %1\BoogieBasetypes.dll .
+copy %1\BoogieBasetypes.pdb .
+copy %1\BoogieCodeContractsExtender.dll .
+copy %1\BoogieCodeContractsExtender.pdb .
+copy %1\BoogieConcurrency.dll .
+copy %1\BoogieConcurrency.pdb .
+copy %1\BoogieCore.dll .
+copy %1\BoogieCore.pdb .
+copy %1\BoogieExecutionEngine.dll .
+copy %1\BoogieExecutionEngine.pdb .
+copy %1\BoogieGraph.dll .
+copy %1\BoogieGraph.pdb .
+copy %1\BoogieHoudini.dll .
+copy %1\BoogieHoudini.pdb .
+copy %1\BoogieModel.dll .
+copy %1\BoogieModel.pdb .
+copy %1\BoogieParserHelper.dll .
+copy %1\BoogieParserHelper.pdb .
+copy %1\Provers.SMTLib.dll .
+copy %1\Provers.SMTLib.pdb .
+copy %1\BoogieVCExpr.dll .
+copy %1\BoogieVCExpr.pdb .
+copy %1\BoogieVCGeneration.dll .
+copy %1\BoogieVCGeneration.pdb .
+REM ------ other boogie files
+REM copy %1\boogie.exe .
+REM copy %1\boogie.pdb .
+REM copy %1\BVD.exe .
+REM copy %1\BVD.pdb .
+REM copy %1\BoogieDoomed.dll .
+REM copy %1\BoogieDoomed.pdb .
+REM copy %1\BoogiePredication.dll .
+REM copy %1\BoogiePredication.pdb .
